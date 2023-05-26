4eac17774dd784bf2a1b06f0c8fec0b864aa371a
Emad Uddin <emaduddin678@gmail.com>
2023-05-27 00:07:42 +0600

    first

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import "./components/styles/App.css"
+import Layout from './components/Layout'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Layout >
+
+      </Layout>
+    </>
+  )
+}
+
+export default App
